519af7a8d5d96d3b6192a29777ca1624ffd81056
Valeria Kasatkina <orionella@gmail.com>
2017-02-01 10:53:22 -0800

    Исправления к задаче с гистограммой

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var getColumnColor = function(name) {
+var getColumnColor = function (name) {
   if (name === 'Вы') {
     return 'rgba(255, 0, 0, 1)';
   }
   var alphaChannel = (0.1 + 0.9 * Math.random()).toFixed(1);
-  return ['rgba(0, 0, 255,', alphaChannel, ')'].join('');
+  return 'rgba(0, 0, 255,' + alphaChannel + ')';
 };
 
+
 window.renderStatistics = function (ctx, names, times) {
+
   var drawRect = function (color, x, y, width, height) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, width, height);
@@ -17,43 +19,37 @@ window.renderStatistics = function (ctx, names, times) {
   drawRect('rgba(0, 0, 0, 0.7)', 110, 20, 420, 270);
   drawRect('white', 100, 10, 420, 270);
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = 'black';
-  ctx.fillText('Ура вы победили!', 170, 40);
-  ctx.fillText('Список результатов', 170, 60);
-  console.log(names);
-  console.log(times);
+  var drawText = function (font, color, text, x, y) {
+    ctx.font = font;
+    ctx.fillStyle = color;
+    ctx.fillText(text, x, y);
+  };
+
+  drawText('16px PT Mono', 'black', 'Ура вы победили!', 170, 40);
+  drawText('16px PT Mono', 'black', 'Список результатов', 170, 60);
 
   var columnHeightMax = 150;
   var columnWidth = 40;
   var columnDistance = 50;
   var timeMax = -1;
 
-
-  for (var i = 0; i < names.length; i++) {
-    if (times[i] > timeMax) {
-      timeMax = times[i];
+  var getTimeMax = function (times) {
+    for (var i = 0; i < times.length; i++) {
+      if (times[i] > timeMax) {
+        timeMax = times[i];
+      }
     }
-  }
+    return timeMax;
+  };
 
-  var rate;
-  if (timeMax > 0) {
-    rate = columnHeightMax / timeMax;
-  } else {
-    rate = 0;
-  }
+  var rate = getTimeMax(times) > 0 ? columnHeightMax / getTimeMax(times) : 0;
 
   for (var i = 0; i < names.length; i++) {
     var columnLeft = 130 + (columnDistance + columnWidth)*i;
     var columnHeight = times[i] * rate;
     drawRect(getColumnColor(names[i]), columnLeft, 250, columnWidth, -columnHeight);
 
-    ctx.font = '16px PT Mono';
-    ctx.fillStyle = 'black';
-    ctx.fillText(times[i].toFixed(0), columnLeft, 250 - columnHeight - 10);
-    ctx.fillText(names[i], columnLeft, 270);
+    drawText('16px PT Mono', 'black', times[i].toFixed(0), columnLeft, 250 - columnHeight - 10);
+    drawText('16px PT Mono', 'black', names[i], columnLeft, 270);
   }
 };
-
-
-
