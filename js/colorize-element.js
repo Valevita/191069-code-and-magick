f001b886b08fc1b3f3676d63b66189a9886d8f6c
Valeria Kasatkina <orionella@gmail.com>
2017-02-15 11:42:02 -0800

    Исправления по инкапсуляции

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,35 +1,5 @@
 'use strict';
 
-/* window.colorizeElement = function (element, colors, property) {
-  var currentColor = null;
-  var setCurrentColor = function () {
-    currentColor = window.utils.getRandomElementExcept(colors, currentColor);
-    element.style[property] = currentColor;
-  };
-  element.addEventListener('click', setCurrentColor);
-  element.addEventListener('keydown', function (event) {
-    if (window.utils.isEventActivated(event)) {
-      setCurrentColor();
-    }
-  });
-};*/
-
-/* window.colorizeElement = (function () {
-  return function (element, colors, property) {
-    var currentColor = null;
-    var setCurrentColor = function () {
-      currentColor = window.utils.getRandomElementExcept(colors, currentColor);
-      element.style[property] = currentColor;
-    };
-    element.addEventListener('click', setCurrentColor);
-    element.addEventListener('keydown', function (event) {
-      if (window.utils.isEventActivated(event)) {
-        setCurrentColor();
-      }
-    });
-  };
-})();*/
-
 window.colorizeElement = (function () {
   var currentColor = null;
   var setCurrentColor = function (element, colors, property) {
--- js/utils.js
+++ js/utils.js
@@ -1,31 +1,5 @@
 'use strict';
 
-/* window.utils = {};
-
-var randomNumber = function (maxValue) {
-  return Math.floor(Math.random() * maxValue);
-};
-
-window.utils.isEventActivated = (function () {
-  var ENTER_KEY_CODE = 13;
-  return function (event) {
-    return event.keyCode && event.keyCode === ENTER_KEY_CODE;
-  };
-})();
-
-
-window.utils.getRandomElement = function (array) {
-  return array[randomNumber(array.length)];
-};
-
-window.utils.getRandomElementExcept = function (array, color) {
-  var newColor;
-  do {
-    newColor = window.utils.getRandomElement(array);
-  } while (!newColor || color === newColor);
-  return newColor;
-};*/
-
 window.utils = (function () {
   var ENTER_KEY_CODE = 13;
 
