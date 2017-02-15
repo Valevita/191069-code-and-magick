8fc97905e84bba84308e32e8ebb84f7573afa18c
Valeria Kasatkina <orionella@gmail.com>
2017-02-14 21:52:59 -0800

    Инкапсуляция

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.colorizeElement = function (element, colors, property) {
+/* window.colorizeElement = function (element, colors, property) {
   var currentColor = null;
   var setCurrentColor = function () {
     currentColor = window.utils.getRandomElementExcept(colors, currentColor);
@@ -8,8 +8,44 @@ window.colorizeElement = function (element, colors, property) {
   };
   element.addEventListener('click', setCurrentColor);
   element.addEventListener('keydown', function (event) {
-    if (event.keyCode === window.ENTER_KEY_CODE) {
+    if (window.utils.isEventActivated(event)) {
       setCurrentColor();
     }
   });
-};
+};*/
+
+/* window.colorizeElement = (function () {
+  return function (element, colors, property) {
+    var currentColor = null;
+    var setCurrentColor = function () {
+      currentColor = window.utils.getRandomElementExcept(colors, currentColor);
+      element.style[property] = currentColor;
+    };
+    element.addEventListener('click', setCurrentColor);
+    element.addEventListener('keydown', function (event) {
+      if (window.utils.isEventActivated(event)) {
+        setCurrentColor();
+      }
+    });
+  };
+})();*/
+
+window.colorizeElement = (function () {
+  var currentColor = null;
+  var setCurrentColor = function (element, colors, property) {
+    currentColor = window.utils.getRandomElementExcept(colors, currentColor);
+    element.style[property] = currentColor;
+  };
+  return function (element, colors, property) {
+    element.addEventListener('click', function () {
+      setCurrentColor(element, colors, property);
+    });
+    element.addEventListener('keydown', function (event) {
+      if (window.utils.isEventActivated(event)) {
+        setCurrentColor(element, colors, property);
+      }
+    });
+  };
+})();
+
+
--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,18 @@
 'use strict';
 
-window.utils = {};
-
-window.ENTER_KEY_CODE = 13;
+/* window.utils = {};
 
 var randomNumber = function (maxValue) {
   return Math.floor(Math.random() * maxValue);
 };
 
-window.utils.isEventActivated = function (event) {
-  return event.keyCode && event.keyCode === window.ENTER_KEY_CODE;
-};
+window.utils.isEventActivated = (function () {
+  var ENTER_KEY_CODE = 13;
+  return function (event) {
+    return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+  };
+})();
+
 
 window.utils.getRandomElement = function (array) {
   return array[randomNumber(array.length)];
@@ -22,5 +24,31 @@ window.utils.getRandomElementExcept = function (array, color) {
     newColor = window.utils.getRandomElement(array);
   } while (!newColor || color === newColor);
   return newColor;
-};
+};*/
+
+window.utils = (function () {
+  var ENTER_KEY_CODE = 13;
+
+  var randomNumber = function (maxValue) {
+    return Math.floor(Math.random() * maxValue);
+  };
+
+  var getRandomElement = function (array) {
+    return array[randomNumber(array.length)];
+  };
+
+  return {
+    isEventActivated: function (event) {
+      return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+    },
+
+    getRandomElementExcept: function (array, color) {
+      var newColor;
+      do {
+        newColor = getRandomElement(array);
+      } while (!newColor || color === newColor);
+      return newColor;
+    }
+  };
+})();
 
